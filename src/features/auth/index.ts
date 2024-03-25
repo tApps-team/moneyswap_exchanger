6bf04d776069e63d96d6bb6c8319a852a49840ae
Dania <danila.lusuy@gmail.com>
2024-03-25 21:00:29 +0300

    feat: add public api

---
+++ src/features/auth/index.ts
@@ -0,0 +1,1 @@
+export { AuthByUserNameForm } from "./authByUserName";
---
+++ src/shared/ui/index.ts
@@ -0,0 +1,19 @@
+export { Button } from "./button";
+export {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+  useFormField,
+} from "./form";
+export { Label } from "./label";
+export { Input } from "./input";
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./tooltip";
