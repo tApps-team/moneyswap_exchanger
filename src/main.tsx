e2316391f49314f7cbe86c1f3ac31b11b182755e
Dania <danila.lusuy@gmail.com>
2024-03-25 21:00:15 +0300

    feat: add AuthByUserName ui component

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./app";
 import "@/shared/styles/global.scss";
+import { AuthByUserNameForm } from "./features/auth";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <AuthByUserNameForm />
+    </>
   </React.StrictMode>
 );
