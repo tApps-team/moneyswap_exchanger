f7944473aa48e4143a60b275bcede7b06c8f621b
SenezAr <danila.lusuy@gmail.com>
2024-03-26 17:32:24 +0300

    feat: add changePasswordForm

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import * as ReactDOM from "react-dom/client";
 import App from "./app";
 import "@/shared/styles/global.scss";
 import { AuthByUserNameForm } from "./features/auth";
+import { ChangePasswordForm } from "./features/auth/changePassword";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <>
       <App />
-      <AuthByUserNameForm />
+      {/* <AuthByUserNameForm /> */}
+      <ChangePasswordForm />
     </>
   </React.StrictMode>
 );
