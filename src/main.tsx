767fa09e155fe5ae20bf57d718847bf2262f7e93
Dania <danila.lusuy@gmail.com>
2024-03-26 21:35:23 +0300

    fix: remove mock data

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
+import "@/shared/styles/global.scss";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import App from "./app";
-import "@/shared/styles/global.scss";
-import { AuthByUserNameForm } from "./features/auth";
-import { ChangePasswordForm } from "./features/auth/changePassword";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <>
-      <App />
-      {/* <AuthByUserNameForm /> */}
-      <ChangePasswordForm />
-    </>
+    <App />
   </React.StrictMode>
 );
