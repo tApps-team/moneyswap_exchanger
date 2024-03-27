3e72dd714fc2208ced3964320696f803e10e960e
Dania <danila.lusuy@gmail.com>
2024-03-27 13:05:39 +0300

    feat: change path

--- src/app/providers/withRouter.tsx
+++ src/app/providers/withRouter.tsx
@@ -1,6 +1,6 @@
 // import { BrowserRouter } from "react-router-dom";
 
-import { router } from "@pages/router";
+import { router } from "@/pages/router";
 import { RouterProvider } from "react-router-dom";
 
 // export const withRouter = (Component: React.FC) => {
