d18308cb2497eaee59d627b841e332b39bff3759
SenezAr <danila.lusuy@gmail.com>
2024-03-28 17:19:52 +0300

    feat: add widget MyCities

---
+++ src/widgets/myDirection/ui/myDirection.tsx
@@ -0,0 +1,10 @@
+import { CreateDirection } from "@/features/direction";
+
+export const MyDirection = () => {
+  return (
+    <div>
+      <div>Мои направления</div>
+      <CreateDirection />
+    </div>
+  );
+};
