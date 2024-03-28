58605b375bf704438a008eb06045a91bffbe00f2
Dania <danila.lusuy@gmail.com>
2024-03-28 11:51:34 +0300

    feat: add AddCityButton component

--- src/pages/home/ui/homePage.tsx
+++ src/pages/home/ui/homePage.tsx
@@ -1,8 +1,10 @@
 import { CityCarousel } from "@/features/city";
+import { AddCityButton } from "@/features/city/addCityButton";
 
 export const HomePage = () => {
   return (
-    <div>
+    <div className="flex gap-2">
+      <AddCityButton />
       <CityCarousel />
     </div>
   );
