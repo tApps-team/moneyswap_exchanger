7c5a0d59756ff34bbbc06e9b21851e0923a4dad9
Dania <danila.lusuy@gmail.com>
2024-03-31 22:30:53 +0300

    feat: add AuthByUserNameForm

---
+++ src/pages/login/ui/loginPage.tsx
@@ -0,0 +1,13 @@
+import { AuthByUserNameForm } from "@/features/auth";
+import { paths } from "@/shared/routing";
+import { Navigate } from "react-router-dom";
+
+export const LoginPage = () => {
+  const isAuth = false;
+
+  return (
+    <div>
+      <AuthByUserNameForm /> {isAuth && <Navigate to={paths.profile} replace />}
+    </div>
+  );
+};
