cda899b29fd7af37e2c4e2b9f877a9103d745f61
Dania <danila.lusuy@gmail.com>
2024-04-02 10:57:23 +0300

    feat: add time select widget

--- src/pages/home/ui/homePage.tsx
+++ src/pages/home/ui/homePage.tsx
@@ -1,17 +1,15 @@
-import { DirectionCard } from "@/entities/direction";
+import { Direction } from "@/features/direction";
 import { MyCities } from "@/widgets/myCities";
-import { MyCustomAuthReq } from "@/widgets/myCustomAuthReq/ui/myCustomAuthReq";
 import { MyDirection } from "@/widgets/myDirection";
+import { TimeSelect } from "@/widgets/timeSelect";
 
 export const HomePage = () => {
   return (
     <div className="grid grid-cols-1 grid-rows-2 gap-4">
       <MyCities />
       <MyDirection />
-      <DirectionCard />
+      <Direction />
+      <TimeSelect />
     </div>
-    //   <div>
-    //   <MyCustomAuthReq />
-    // </div>
   );
 };
