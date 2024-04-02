bf6d71ea4c6739c8654a341a21b92e888ae4ca86
Dania <danila.lusuy@gmail.com>
2024-04-02 10:57:14 +0300

    fix: refactoring

---
+++ src/features/direction/ui/direction.tsx
@@ -0,0 +1,11 @@
+import { DirectionCard } from "@/entities/direction";
+import { DeleteDirection } from "../deleteDirection";
+import { EditDirection } from "../editDirection";
+
+export const Direction = () => {
+  return (
+    <div>
+      <DirectionCard />
+    </div>
+  );
+};
