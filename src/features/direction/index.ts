596708f596d238bf9f20d555a9d1ccb970865fe2
Dania <danila.lusuy@gmail.com>
2024-04-02 10:56:24 +0300

    feat: add public api

--- src/features/direction/index.ts
+++ src/features/direction/index.ts
@@ -1,1 +1,4 @@
 export { CreateDirection } from "./createDirection";
+export { DeleteDirection } from "./deleteDirection";
+export { EditDirection } from "./editDirection";
+export { Direction } from "./ui/direction";
