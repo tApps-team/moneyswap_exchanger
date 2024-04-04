72bf949e3f9f3f2439350313ae01a7d2e5cee35f
SenezAr <danila.lusuy@gmail.com>
2024-04-04 18:16:37 +0300

    feat(test): add List

---
+++ src/features/list/ui/list.tsx
@@ -0,0 +1,16 @@
+import { ReactNode } from "react";
+
+type ListProps<T> = {
+  items: T[];
+  renderItem: (item: T) 
+};
+export const List = <T extends object>(props: ListProps<T>) => {
+  const { items, renderItem } = props;
+  return (
+    <div>
+      {items?.map((item, index) => (
+        <div key={index}>{renderItem(item)}</div>
+      ))}
+    </div>
+  );
+};
