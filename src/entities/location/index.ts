5644d226b43ab29fbd4b008104d9acf04299073a
SenezAr <danila.lusuy@gmail.com>
2024-04-04 18:15:30 +0300

    feat: add public api

--- src/entities/location/index.ts
+++ src/entities/location/index.ts
@@ -1,3 +1,2 @@
-export { CityCard, type CityCardProps } from "./ui/cityCard";
 export type { City, Country } from "./model/types";
-export { CountryCard } from "./ui/countryCard";
+export { LocationCard } from "./ui/locationCard";
