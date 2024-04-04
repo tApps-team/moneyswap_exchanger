4c4f9c1ae53e7023f6e9a8ecdd5100d6dda737f0
SenezAr <danila.lusuy@gmail.com>
2024-04-04 18:16:20 +0300

    feat: add location card

--- src/entities/location/ui/locationCard.tsx
+++ src/entities/location/ui/locationCard.tsx
@@ -1,16 +1,23 @@
 import { Card, CardContent } from "@/shared/ui";
 import { City, Country } from "../model/types";
 
-type LocationCardProps = Country &
-  City & {
-    onClick: () => void;
-  };
+type LocationCardProps = Partial<
+  Country &
+    City & {
+      onClick: () => void;
+    }
+>;
 export const LocationCard = (props: LocationCardProps) => {
-  const { code_name, id, name, onClick, country_icon } = props;
+  const { code_name, id, name, onClick, country_flag } = props;
   return (
-    <Card onClick={onClick} className="cursor-pointer">
-      <CardContent className="flex gap-2">
-        <div>image</div>
+    <Card
+      onClick={onClick}
+      className="p-4 cursor-pointer rounded-full border-2 border-white bg-gray-600"
+    >
+      <CardContent className="p-0 flex gap-2 items-center  text-white">
+        {country_flag && (
+          <img src={country_flag} alt={`country flag ${name}`} />
+        )}
         <div>{name}</div>
       </CardContent>
     </Card>
