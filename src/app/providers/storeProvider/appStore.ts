e282aa99e41b702ef5ba47e134b7dd8cac2e65dd
Dania <danila.lusuy@gmail.com>
2024-04-05 13:25:29 +0300

    feat: add base api

--- src/app/providers/storeProvider/appStore.ts
+++ src/app/providers/storeProvider/appStore.ts
@@ -1,17 +1,19 @@
 import { userSlice } from "@/entities/user";
 import { authAPI } from "@/entities/user/api/authService";
+import { baseApi } from "@/shared/api";
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 const rootReducer = combineReducers({
   [userSlice.name]: userSlice.reducer,
   [authAPI.reducerPath]: authAPI.reducer,
+  [baseApi.reducerPath]: baseApi.reducer,
 });
 
 export const setupStore = () => {
   return configureStore({
     reducer: rootReducer,
     middleware: (getDefaultMiddleware) =>
-      getDefaultMiddleware().concat(authAPI.middleware),
+      getDefaultMiddleware().concat(authAPI.middleware, baseApi.middleware),
   });
 };
 
