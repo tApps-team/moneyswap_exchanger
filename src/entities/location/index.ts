e085bbc808ec40488c65bac640c5d0980d3247ec
Dania <danila.lusuy@gmail.com>
2024-04-05 13:25:51 +0300

    feat: add public api

--- src/entities/location/index.ts
+++ src/entities/location/index.ts
@@ -1,2 +1,7 @@
 export type { City, Country } from "./model/types";
 export { LocationCard } from "./ui/locationCard";
+export {
+  locationApi,
+  useAllCountriesQuery,
+  useCitiesByCountryNameQuery,
+} from "./api/locationApi";
