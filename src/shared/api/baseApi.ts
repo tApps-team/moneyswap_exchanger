5c7bb29e065cba02bc0cf707dd197663c8686993
Dania <danila.lusuy@gmail.com>
2024-04-05 13:27:49 +0300

    feat: add baseApi, instanse rtk query

---
+++ src/shared/api/baseApi.ts
@@ -0,0 +1,10 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const baseApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+  }),
+  reducerPath: "api",
+  endpoints: () => ({}),
+  tagTypes: [],
+});
