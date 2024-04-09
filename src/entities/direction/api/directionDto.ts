500d00f94b860c28e1ca8922a0459d93d42bfd14
SenezAr <danila.lusuy@gmail.com>
2024-04-09 16:57:42 +0300

    feat: update dto

--- src/entities/direction/api/directionDto.ts
+++ src/entities/direction/api/directionDto.ts
@@ -5,7 +5,14 @@ export type AvailableValutesDtoRequest = {
 };
 export type AvailableValutesDtoResponse = CurrencyCategory;
 
-export type ActualCourseDtoResponse = string;
+export type ActualCourseDtoResponse = {
+  valute_from: string;
+  icon_valute_from: string;
+  in_count: string;
+  valute_to: string;
+  icon_valute_to: string;
+  out_count: string;
+};
 export type ActualCourseDtoRequest = {
   valute_from: string;
   valute_to: string;
