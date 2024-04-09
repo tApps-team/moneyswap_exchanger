59c7e195a56c8fa304e750c90af523ae398a1994
Dania <danila.lusuy@gmail.com>
2024-04-09 10:35:55 +0300

    feat: add endpoints

--- src/entities/direction/api/directionService.ts
+++ src/entities/direction/api/directionService.ts
@@ -1,11 +1,37 @@
 import { Direction } from "../model/types";
 import { authApi } from "@/shared/api";
+import {
+  ActualCourseDtoRequest,
+  ActualCourseDtoResponse,
+  AvailableValutesDtoRequest,
+  AvailableValutesDtoResponse,
+} from "./directionDto";
 
 export const directionAPI = authApi.injectEndpoints({
   endpoints: (build) => ({
     directionsByCity: build.query<Direction[], string>({
       query: (codeName) => `partner/directions_by_city?code_name=${codeName}`,
     }),
+    availableValutes: build.query<
+      AvailableValutesDtoResponse,
+      AvailableValutesDtoRequest
+    >({
+      query: ({ base = "all" }) => ({
+        url: `partner/available_valutes?base=${base}`,
+        method: "GET",
+      }),
+      // transformResponse: (response:AvailableValutesDtoResponse ) => response
+    }),
+    actualCourse: build.query<ActualCourseDtoResponse, ActualCourseDtoRequest>({
+      query: ({ valute_from, valute_to }) => ({
+        url: `actual_course?valute_from=${valute_from}&valute_to=${valute_to}`,
+        method: "GET",
+      }),
+    }),
   }),
 });
-export const { useDirectionsByCityQuery } = directionAPI;
+export const {
+  useDirectionsByCityQuery,
+  useAvailableValutesQuery,
+  useActualCourseQuery,
+} = directionAPI;
