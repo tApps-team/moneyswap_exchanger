7adf4df7cf5ef4ff35a64def6252be4e7e123273
SenezAr <danila.lusuy@gmail.com>
2024-04-10 17:51:48 +0300

    fix: change url

--- src/entities/direction/api/directionService.ts
+++ src/entities/direction/api/directionService.ts
@@ -24,7 +24,7 @@ export const directionAPI = authApi.injectEndpoints({
     }),
     actualCourse: build.query<ActualCourseDtoResponse, ActualCourseDtoRequest>({
       query: ({ valute_from, valute_to }) => ({
-        url: `actual_course?valute_from=${valute_from}&valute_to=${valute_to}`,
+        url: `/partner/actual_course?valute_from=${valute_from}&valute_to=${valute_to}`,
         method: "GET",
       }),
     }),
