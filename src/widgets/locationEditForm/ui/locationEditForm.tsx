74606958c0da183cb06cb60579a94b993d527428
Dania <danila.lusuy@gmail.com>
2024-04-12 15:36:16 +0300

    feat: add locationEditForm

---
+++ src/widgets/locationEditForm/ui/locationEditForm.tsx
@@ -0,0 +1,6 @@
+import { useAppSelector } from "@/shared/model";
+
+export const LocationEditForm = () => {
+  const activeCity = useAppSelector((state) => state.activeCity.activeCity);
+  return <div>\</div>;
+};
