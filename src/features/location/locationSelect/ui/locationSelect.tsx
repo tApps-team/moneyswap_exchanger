a79d45b3e021080cb06fc793f8e68b55c7c9e884
Dania <danila.lusuy@gmail.com>
2024-04-14 22:02:00 +0300

    fix: change styles

--- src/features/location/locationSelect/ui/locationSelect.tsx
+++ src/features/location/locationSelect/ui/locationSelect.tsx
@@ -4,25 +4,26 @@ import {
   Drawer,
   DrawerClose,
   DrawerContent,
-  DrawerDescription,
   DrawerHeader,
-  DrawerTitle,
   DrawerTrigger,
   Input,
   ScrollArea,
 } from "@/shared/ui";
-import { ChevronDown, Circle } from "lucide-react";
-import { useState } from "react";
+import { ChevronDown, Search } from "lucide-react";
+import { useMemo, useState } from "react";
 
 type SelectCities = {
   type: "city";
-  city: City[];
-  onClick: (city: City) => void;
+  city?: City[];
+  inputPlaceholderCity?: string;
+  onClick?: (city: City) => void;
 };
 type SelectCountry = {
   type: "country";
   country?: Country[];
-  onClick: (country: Country) => void;
+  countryIcon?: string;
+  inputPlaceholderCountry?: string;
+  onClick?: (country: Country) => void;
 };
 type LocationSelectProps = {
   label?: string;
@@ -32,41 +33,49 @@ type LocationSelectProps = {
 export const LocationSelect = (props: LocationSelectProps) => {
   const { type, label, disabled } = props;
   const [searchValue, setSearchValue] = useState<string>("");
+
   const cityItem = type === "city" ? props.city : null;
   const countryItem = type === "country" ? props.country : null;
 
   const emptyLabel = type === "city" ? "Выберите город" : "Выберите страну";
 
-  const locationItems =
+  const placeholder =
     type === "city"
-      ? cityItem
-          ?.filter((city) =>
-            city.name.toLowerCase().includes(searchValue.toLowerCase())
-          )
-          .map((city) => (
-            <DrawerClose key={city.id}>
-              <LocationCard
-                code_name={city.code_name}
-                id={city.id}
-                name={city.name}
-                onClick={() => props.onClick(city)}
-              />
-            </DrawerClose>
-          ))
-      : countryItem
-          ?.filter((country) =>
-            country.name.toLowerCase().includes(searchValue.toLowerCase())
-          )
-          .map((country) => (
-            <DrawerClose key={country.id}>
-              <LocationCard
-                country_flag={country.country_flag}
-                id={country.id}
-                name={country.name}
-                onClick={() => props.onClick(country)}
-              />
-            </DrawerClose>
-          ));
+      ? props.inputPlaceholderCity
+      : props.inputPlaceholderCountry;
+  const locationItems = useMemo(
+    () =>
+      type === "city"
+        ? cityItem
+            ?.filter((city) =>
+              city.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((city) => (
+              <DrawerClose key={city.id}>
+                <LocationCard
+                  code_name={city.code_name}
+                  id={city.id}
+                  name={city.name}
+                  onClick={() => props.onClick && props?.onClick(city)}
+                />
+              </DrawerClose>
+            ))
+        : countryItem
+            ?.filter((country) =>
+              country.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
+            .map((country) => (
+              <DrawerClose key={country.id}>
+                <LocationCard
+                  country_flag={country.country_flag}
+                  id={country.id}
+                  name={country.name}
+                  onClick={() => props.onClick && props.onClick(country)}
+                />
+              </DrawerClose>
+            )),
+    [cityItem, countryItem, props, searchValue, type]
+  );
 
   return (
     <div>
@@ -74,11 +83,18 @@ export const LocationSelect = (props: LocationSelectProps) => {
         <DrawerTrigger asChild>
           <Button
             disabled={disabled}
-            className="w-full justify-between items-center rounded-full gap-2 select-none"
+            className="w-full  text-white hover:bg-mainColor disabled:pointer-events-none bg-darkGray h-14 disabled:bg-lightGray justify-between items-center rounded-full gap-2 select-none"
             variant={"outline"}
           >
             <div className="flex gap-2 items-center">
-              <Circle />
+              {type === "country" && props.countryIcon ? (
+                <img
+                  width={32}
+                  height={32}
+                  src={props.countryIcon}
+                  alt={`country ${props.label}`}
+                />
+              ) : null}
               <div>{label ? label : emptyLabel}</div>
             </div>
             <div className="flex ">
@@ -90,12 +106,15 @@ export const LocationSelect = (props: LocationSelectProps) => {
         <DrawerContent className="h-screen">
           <DrawerHeader>
             <Input
+              startAdornment={<Search className="translate-y-8 ml-2" />}
+              className="rounded-xl bg-lightGray text-darkGray pl-10 focus-visible:ring-transparent focus-visible:ring-offset-0"
               value={searchValue}
+              placeholder={placeholder || ""}
               onChange={(e) => setSearchValue(e.target.value.trim())}
             />
           </DrawerHeader>
           <div className="p-4">
-            <ScrollArea className="h-[320px] w-full ">
+            <ScrollArea className="h-[420px] w-full ">
               <div className="flex flex-col gap-4">{locationItems}</div>
             </ScrollArea>
           </div>
@@ -104,3 +123,75 @@ export const LocationSelect = (props: LocationSelectProps) => {
     </div>
   );
 };
+// type LocationSelectRefatoringProps = {
+//   locationItems: Partial<City | Country>[];
+//   label?: string;
+//   disabled?: boolean;
+//   emptyLabel?: string;
+//   locationIcon: string;
+//   onClick: (location: Partial<City & Country>) => void;
+// };
+// export const LocationSelectRefatoring = (
+//   props: LocationSelectRefatoringProps
+// ) => {
+//   const { locationItems, disabled, label, onClick, locationIcon, emptyLabel } =
+//     props;
+//   const [searchValue, setSearchValue] = useState<string>("");
+//   const filteredLocationItems = useMemo(
+//     () =>
+//       locationItems.filter((item) =>
+//         item.name?.toLowerCase().includes(searchValue.toLowerCase())
+//       ),
+//     []
+//   );
+//   return (
+//     <Drawer>
+//       <DrawerTrigger asChild>
+//         <Button
+//           disabled={disabled}
+//           className="w-full text-white hover:bg-mainColor disabled:pointer-events-none bg-darkGray h-14 disabled:bg-lightGray justify-between items-center rounded-full gap-2 select-none"
+//           variant={"outline"}
+//         >
+//           <div className="flex gap-2 items-center">
+//             {locationIcon && (
+//               <img
+//                 width={32}
+//                 height={32}
+//                 src={locationIcon}
+//                 alt={"country icon"}
+//               />
+//             )}
+//             <div>{label ? label : emptyLabel}</div>
+//           </div>
+//           <div className="flex ">
+//             <div>change</div>
+//             <ChevronDown />
+//           </div>
+//         </Button>
+//       </DrawerTrigger>
+//       <DrawerContent className="h-screen">
+//         <DrawerHeader>
+//           <Input
+//             value={searchValue}
+//             onChange={(e) => setSearchValue(e.target.value.trim())}
+//           />
+//         </DrawerHeader>
+//         <div className="p-4">
+//           <ScrollArea className="h-[420px] w-full ">
+//             <div className="flex flex-col gap-4">
+//               {filteredLocationItems.map((item) => (
+//                 <LocationCard
+//                   code_name={item.code_name}
+//                   country_flag={item.country_flag}
+//                   id={item.id}
+//                   name={item.name}
+//                   onClick={() => onClick(item)}
+//                 />
+//               ))}
+//             </div>
+//           </ScrollArea>
+//         </div>
+//       </DrawerContent>
+//     </Drawer>
+//   );
+// };
