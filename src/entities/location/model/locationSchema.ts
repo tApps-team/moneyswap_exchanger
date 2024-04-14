94c10597c8ae9196aff583e2ff839a7761111705
Dania <danila.lusuy@gmail.com>
2024-04-14 22:01:21 +0300

    feat: add schema

--- src/entities/location/model/locationSchema.ts
+++ src/entities/location/model/locationSchema.ts
@@ -31,3 +31,21 @@ export const locationSchema = z.object({
   workDays: z.record(z.string(), z.boolean()),
 });
 export type LocationSchemaType = z.infer<typeof locationSchema>;
+
+export const locationEditSchema = z.object({
+  city: z.object({
+    name: z.string(),
+    code_name: z.string(),
+  }),
+  country: z.object({
+    name: z.string(),
+    country_flag: z.string(),
+  }),
+  deliviry: z.boolean(),
+  office: z.boolean(),
+  timeStart: z.string(),
+  timeEnd: z.string(),
+  workDays: z.record(z.string(), z.boolean()),
+});
+
+export type LocationEditSchemaType = z.infer<typeof locationEditSchema>;
