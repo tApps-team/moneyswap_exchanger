365c8f53e00280c3508666e7ba2cf5521a392af3
SenezAr <danila.lusuy@gmail.com>
2024-04-15 17:16:52 +0300

    feat: add icon

---
+++ src/shared/assets/icons/logoButtonIcon.tsx
@@ -0,0 +1,17 @@
+export const LogoButtonIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    id="_\u0421\u043B\u043E\u0439_1"
+    data-name="\u0421\u043B\u043E\u0439 1"
+    viewBox="0 0 74.77 59.77"
+    {...props}
+  >
+    <defs>
+      <style>{".cls-1{fill:#f6ff5f;stroke-width:0}"}</style>
+    </defs>
+    <path
+      d="M.006 29.877 10.563 19.32l19.325 19.325L19.33 49.202zM44.85 59.77v-.04l.04.04 29.88-29.89-10.56-10.56-19.36 19.36V0H29.92v59.77h14.93z"
+      className="cls-1"
+    />
+  </svg>
+);
