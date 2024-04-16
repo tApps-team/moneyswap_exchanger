35abae25305fd30c48889ac9b76a16a41b758612
Dania <danila.lusuy@gmail.com>
2024-04-16 10:33:16 +0300

    feat: add Toaster

--- src/pages/layouts/mainLayout/ui/mainLayout.tsx
+++ src/pages/layouts/mainLayout/ui/mainLayout.tsx
@@ -1,11 +1,13 @@
 import { ReactNode } from "react";
 import styles from "./mainLayout.module.scss";
 import { PathFooter } from "@/widgets/pathFooter";
+import { Toaster } from "@/shared/ui/toast";
 
 export const MainLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className={styles.main__container}>
       <div className={styles.main__content}>{children}</div>
+      <Toaster />
       <footer className={styles.main__footer}>
         <PathFooter />
       </footer>
