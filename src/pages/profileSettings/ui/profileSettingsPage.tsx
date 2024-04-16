8e4b9995967c66f8e29cd59455114e9b16895af9
SenezAr <danila.lusuy@gmail.com>
2024-04-16 18:12:17 +0300

    feat: add styles

--- src/pages/profileSettings/ui/profileSettingsPage.tsx
+++ src/pages/profileSettings/ui/profileSettingsPage.tsx
@@ -6,24 +6,30 @@ import { Link } from "react-router-dom";
 export const ProfileSettingsPage = () => {
   return (
     <div className="flex flex-col gap-4">
-      <Button className="justify-start gap-4 cursor-pointer" asChild>
+      <Button
+        className="justify-start gap-4 bg-transparent cursor-pointer"
+        asChild
+      >
         <Link
           to={`${paths.profile}${paths.profileSettings}${paths.profileInfo}`}
         >
-          <User />
+          <User color="#F6FF5F" />
           <div className="flex flex-col">
-            <div>Информация аккаунта</div>
-            <div>Измените вашу информацию</div>
+            <div>ИНФОРМАЦИЯ АККАУНТА</div>
+            <div>Измените вагу информацию</div>
           </div>
         </Link>
       </Button>
-      <Button className="justify-start gap-4 cursor-pointer" asChild>
+      <Button
+        className="justify-start bg-transparent gap-4 cursor-pointer"
+        asChild
+      >
         <Link
           to={`${paths.profile}${paths.profileSettings}${paths.profilePassword}`}
         >
-          <Eye />
+          <Eye color="#F6FF5F" />
           <div className="flex flex-col">
-            <div>Пароль</div>
+            <div>ПАРОЛЬ</div>
             <div>Измените ваш пароль</div>
           </div>
         </Link>
