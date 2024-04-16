8e073d365df10911c21a90a496a28a6a13a33f72
SenezAr <danila.lusuy@gmail.com>
2024-04-16 18:12:47 +0300

    feat: add eye props

--- src/shared/ui/input/passwordInput.tsx
+++ src/shared/ui/input/passwordInput.tsx
@@ -12,9 +12,10 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {}
 export type PasswordInputProps = InputProps & {
   endIcon?: React.ReactNode;
+  eyeIcon?: boolean;
 };
 const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
-  ({ className, type, endIcon, ...props }, ref) => {
+  ({ className, type, eyeIcon, endIcon, ...props }, ref) => {
     const [showPassword, setShowPassword] = React.useState(false);
 
     const togglePasswordVisibility = () => {
@@ -25,27 +26,30 @@ const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
 
     return (
       <div className="relative">
-        <Lock className="absolute left-3 top-1/2 -translate-y-1/2 " />
+        <Lock
+          color="#F6FF5F"
+          className="absolute left-3 top-1/2 -translate-y-1/2 "
+        />
         <input
           type={inputType}
           className={cn(
-            "flex h-10 w-full rounded-md border border-input bg-transparent px-12 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            "flex   h-10 w-full rounded-md border border-input bg-transparent px-12 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
             className
           )}
           ref={ref}
           {...props}
         />
-        {!endIcon && type === "password" && (
+        {!endIcon && eyeIcon && type === "password" && (
           <button
             type="button"
-            className="absolute right-3 top-1/2 -translate-y-1/2"
+            className="absolute  right-4 top-1/2 -translate-y-1/2"
             onClick={togglePasswordVisibility}
           >
             {showPassword ? (
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger asChild>
-                    <Eye className="h-4 w-4 cursor-pointer" />
+                    <Eye color="#F6FF5F" className="h-4 w-4 cursor-pointer" />
                   </TooltipTrigger>
                   <TooltipContent>
                     <p>Спрятать пароль</p>
@@ -57,7 +61,7 @@ const PasswordInput = React.forwardRef<HTMLInputElement, PasswordInputProps>(
               <TooltipProvider>
                 <Tooltip>
                   <TooltipTrigger asChild>
-                    <EyeOff className="h-4 w-4 text-gray-500" />
+                    <EyeOff color="#F6FF5F" className="h-4 w-4 text-gray-500" />
                   </TooltipTrigger>
                   <TooltipContent>
                     <p>Показать пароль</p>
