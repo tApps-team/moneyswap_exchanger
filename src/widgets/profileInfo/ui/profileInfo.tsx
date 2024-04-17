de66baf6f0ca084a8274008e65c93a399d7c9c38
Dania <danila.lusuy@gmail.com>
2024-04-17 18:56:34 +0300

    fix: refactoring

--- src/widgets/profileInfo/ui/profileInfo.tsx
+++ src/widgets/profileInfo/ui/profileInfo.tsx
@@ -1,25 +1,36 @@
 import { useProfileInfoQuery } from "@/entities/user";
-import { Button } from "@/shared/ui";
+import { Button, Skeleton } from "@/shared/ui";
 import { Link } from "react-router-dom";
 
 export const ProfileInfo = () => {
-  const { data: profileInfo } = useProfileInfoQuery();
+  const { data: profileInfo, isLoading: isLoadingProfileInfo } =
+    useProfileInfoQuery();
   return (
-    <div className="text-white flex flex-col gap-4">
-      <div>{profileInfo?.title.ru}</div>
-      <div>{profileInfo?.partner_link}</div>
-      <div className="text-center">
-        <div>Свяжитесь с поддержкой</div>
-        <div>для изменений</div>
+    <div className="text-white grid grid-row-3 h-screen ">
+      {isLoadingProfileInfo ? (
+        <div>
+          <Skeleton className="w-full h-16 rounded-xl" />
+        </div>
+      ) : (
+        <div>
+          <div>{profileInfo?.title.ru.toUpperCase()}</div>
+          <div>{profileInfo?.partner_link}</div>
+        </div>
+      )}
+      <div className="grid grid-row-2">
+        <div className="text-center">
+          <div>Свяжитесь с поддержкой</div>
+          <div>для изменений</div>
+        </div>
+        <Button
+          className="row-span-3 rounded-full border border-bg-darkGray h-14 bg-darkGray text-mainColor text-xl"
+          asChild
+        >
+          <Link target="_blank" to={"https://www.google.com/"}>
+            СВЯЗАТЬСЯ
+          </Link>
+        </Button>
       </div>
-      <Button
-        className="rounded-full border border-bg-darkGray h-14 bg-darkGray text-mainColor text-xl"
-        asChild
-      >
-        <Link target="_blank" to={"https://www.google.com/"}>
-          СВЯЗАТЬСЯ
-        </Link>
-      </Button>
     </div>
   );
 };
